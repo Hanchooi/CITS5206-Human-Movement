6ad44bc4235f26cc49a235bfa14e21c8c238e61d
lovedeep14 <79499043+lovedeep14@users.noreply.github.com>
2021-10-10 10:45:56 +0800

    Update merge_visited.sql

--- Data Integrator with GUI/merge_visited.sql
+++ Data Integrator with GUI/merge_visited.sql
@@ -7,11 +7,14 @@
 
 SELECT 
 R.*, CASE
-	WHEN speed >= 25 AND speed <= 28 THEN 'Bus'
-	WHEN speed >= 13 AND speed <= 25 THEN 'Car'
-	WHEN speed >= 1.5 AND speed <= 2.5 THEN 'Walking'
-	WHEN speed >= 2.5 AND speed <= 4.1 THEN'Running'
-	WHEN speed >= 40 AND speed <= 98 THEN 'Train'
+	WHEN speed >= 0.5 AND speed <= 1.1 THEN 'Swimming'
+	WHEN speed >= 1.1 AND speed <= 2.7 THEN 'Walking'
+	WHEN speed >= 2.7 AND speed <= 3.6 THEN 'Running'
+	WHEN speed >= 3.6 AND speed <= 6.9 THEN 'E-scooter'
+	WHEN speed >= 6.9 AND speed <= 11.1 THEN 'Cycling'
+	WHEN speed >= 11.1 AND speed <= 19.2 THEN 'Car'
+	WHEN speed >= 19.2 AND speed <= 20.0 THEN 'Bus'
+	WHEN speed >= 20.0 AND speed <= 36.0 THEN 'Train'
 	ELSE 'Stationary'
 END AS 'mode_of_transport', VP.locationName AS vis_name, 
 VP.Activity1 AS vis_activity1, VP.Activity2 AS vis_activity2, VP.Activity3 AS vis_activity3,VP.latitude AS vis_lat, VP.longitude AS vis_long
